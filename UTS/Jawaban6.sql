6ae3ed5aac88c98ec6a2d93a3ea04a2ee17fd182
faizaput <151031331+faizaput@users.noreply.github.com>
2023-11-16 19:46:55 +0700

    Update Jawaban6.sql

--- UTS/Jawaban6.sql
+++ UTS/Jawaban6.sql
@@ -1,1 +1,4 @@
-
+SELECT d.nama AS nama_dosen, m.nama AS nama_mahasiswa, mk.nama_matkul
+FROM dosen d
+JOIN mahasiswa m ON d.id_dosen = m.id_mahasiswa
+JOIN mata_kuliah mk ON m.id_mahasiswa = mk.id_matkul;
